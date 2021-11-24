8887a3b1557ff01d71e9be5d2c464e6cd5fd22be
cpozo <carlosricardo.pozo@hotmail.com>
2021-11-24 01:19:04 +0100

    Adding input,outputs, services, dbz case

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 1000
+  }
+
+  constructor() {}
+
+
+}
